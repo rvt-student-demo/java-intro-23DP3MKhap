6ca8e83ff6a23f0723cc20e20b1149d94c751647
23DP3MKhap <166131551+23DP3MKhap@users.noreply.github.com>
2024-09-06 11:21:02 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,32 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        // System.out.println("Hello, world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        // String input = scanner.nextLine();
+
+        // System.out.println(input);
+
+        // System.out.println("Sveiks " + input + "!");
+
+        // String name = "John";
+        // int age = 20;
+        // boolean isOnline = true;
+
+        String name = scanner.nextLine();
+        String surname = scanner.nextLine();
+        String grupa = scanner.nextLine();
+
+        System.out.println("Students: " + name + " " + surname + ", " + "grupa: " + grupa);
+
+
 
     }
 
