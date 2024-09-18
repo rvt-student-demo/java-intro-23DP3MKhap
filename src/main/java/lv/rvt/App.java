ae7c1a0bb5e5b0ba919e2d81aac590848de4ebc8
23DP3MKhap <166131551+23DP3MKhap@users.noreply.github.com>
2024-09-18 06:53:32 +0000

    Simple calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,51 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        // // Make your first Hello World app!
+        // String text = "contains text";
+        // int wholeNumber = 123;
+        // double floatingPoint = 3.141592653;
+        // boolean trueOrFalse = true;
 
-    }
+        Scanner scanner = new Scanner(System.in);
 
-}
+        // System.out.println("Text variable: " + text);
+        // System.out.println("Integer variable: " + wholeNumber);
+        // System.out.println("Floating-point variable: " + floatingPoint);
+        // System.out.println("Boolean: " + trueOrFalse); 
+
+
+
+        // System.out.println("Write text and press enter ");
+        // String text = scanner.nextLine();
+        // System.out.println("You wrote " + text);
+
+
+        // int value = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Give the first number:");
+        int x = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int y = Integer.valueOf(scanner.nextLine());
+
+        int plus = x+y;
+        int minuss = x-y;
+        int divide = x*y;
+        double del = (double) x/y;
+
+        System.out.println(x + " + " + y + " = " + plus);
+        System.out.println(x + " - " + y + " = " + minuss);
+        System.out.println(x + " * " + y + " = " + divide);
+        System.out.println(x + " / " + y + " = " + del);
+
+
+
+        
+
+
+    }}
