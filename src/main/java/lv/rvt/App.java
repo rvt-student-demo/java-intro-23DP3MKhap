070bcdb767a4c1264e45e9f4135b0a4ed3cb91ea
23DP3MKhap <166131551+23DP3MKhap@users.noreply.github.com>
2024-11-01 07:01:52 +0000

    Personal details

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,23 +4,48 @@ import java.util.Scanner;
 
 import java.util.ArrayList;
 
+import java.util.Random;
+
+
+
 public class App 
 {
+
+
+
+
     public static void main( String[] args )
+
     {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> skaitlis = new ArrayList<>();
-        int summ = 0;
-        int sk = 0;
-        while(true){
-            sk = scanner.nextInt();
-            skaitlis.add(sk);
-            summ += sk;
-            if(sk == 0){
+
+        double gsum = 0;
+        int maxl = 0;
+        int kl = 0;
+        String maxi = ""; 
+
+        while (true){
+            String i = scanner.nextLine();
+            if (i.equals("")){
                 break;
             }
+        kl ++;
+
+        String [] ch = i.split(",");
+        gsum += Integer.valueOf(ch[1]);
+        
+        if (maxl < ch[0].length()){
+            maxl = ch[0].length();
+            maxi = ch[0];
         }
-        System.out.println(summ);
+
+
+        }
+
+        System.out.println("Longest name: " + maxi);
+        System.out.println("Average of the birth years: " + gsum/kl);
+
+
 
     }
 }
