c217b80a954b6d24fc80fba7b60a0804445049b7
23DP3MKhap <166131551+23DP3MKhap@users.noreply.github.com>
2024-11-08 07:44:50 +0000

    Product

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -17,21 +17,10 @@ public class App
     public static void main( String[] args )
 
     {
-        Account mattAccount = new Account("Matthew's account", 1000.00);
-        Account myAccount = new Account("My account", 0.00);
-        System.out.println("Initial state");
-        System.out.println(mattAccount);
-        System.out.println(myAccount);
-        
-        mattAccount.withdraw(100);
-        System.out.println("The balance of Matthew's account is now: " + mattAccount.balance());
+        Product product1 = new Product("Banana", 1.1, 13);
 
-        myAccount.deposit(100);
-        System.out.println("The balance of My account is now: " + myAccount.balance());
-        
-        System.out.println("End state");
-        System.out.println(myAccount);
-        System.out.println(mattAccount);
+
+        product1.printProduct();
 
     }
 }
